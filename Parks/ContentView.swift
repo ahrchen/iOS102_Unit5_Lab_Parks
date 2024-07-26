f6ab0d69f1cfe15450b04e703d6791e00456b8a1
Raymond Chen <ahrchen@gmail.com>
2024-07-25 17:02:19 -0700

    Initial Commit

---
+++ Parks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Parks
+//
+//  Created by Raymond Chen on 7/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Parks/ParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParksApp.swift
+//  Parks
+//
+//  Created by Raymond Chen on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
