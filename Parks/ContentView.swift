3e38a4e9c6633d7c557c14151ebd30b64a089dde
Raymond Chen <ahrchen@gmail.com>
2024-07-26 20:03:07 -0700

    Create a network request using URLSession with async/await

--- Parks/ContentView.swift
+++ Parks/ContentView.swift
@@ -16,6 +16,25 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            Task {
+                guard let url = URL(string: "https://developer.nps.gov/api/v1/parks?stateCode=wa&api_key={YOUR_API_KEY}") else {
+                    return
+                }
+                do {
+                    let(data, _) = try await URLSession.shared.data(from: url)
+                    
+                    let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+                    
+                    let prettyPrintedData = try JSONSerialization.data(withJSONObject: jsonObject, options: .prettyPrinted)
+                    if let prettyPrintedString = String(data:prettyPrintedData, encoding: .utf8) {
+                        print(prettyPrintedString)
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
 }
 
