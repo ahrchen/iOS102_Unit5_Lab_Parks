d7d260c076c696e4aae6abeed149dc7042810c8a
Raymond Chen <ahrchen@gmail.com>
2024-07-29 19:19:00 -0700

    Refactor the park row into its own view

--- Parks/ContentView.swift
+++ Parks/ContentView.swift
@@ -15,32 +15,7 @@ struct ContentView: View {
         ScrollView {
             LazyVStack {
                 ForEach(parks) { park in
-                    Rectangle()
-                        .aspectRatio(4/3, contentMode: .fit)
-                        .overlay {
-                            let image = park.images.first
-                            let urlString = image?.url
-                            let url = urlString.flatMap { string in
-                                URL(string: string)
-                            }
-                            
-                            AsyncImage(url: url) { image in
-                                image
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                            } placeholder: {
-                                Color(.systemGray4)
-                            }
-                        }
-                        .overlay(alignment: .bottomLeading) {
-                            Text(park.name)
-                                .font(.title)
-                                .bold()
-                                .foregroundStyle(.white)
-                                .padding()
-                        }
-                        .clipShape(.rect(cornerRadius: 16, style: .continuous))
-                        .padding(.horizontal)
+                    ParkRow(park: park)
                 }
             }
         }
@@ -73,4 +48,38 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+struct ParkRow: View {
+    
+    let park: Park
+    
+    var body: some View {
+        Rectangle()
+            .aspectRatio(4/3, contentMode: .fit)
+            .overlay {
+                let image = park.images.first
+                let urlString = image?.url
+                let url = urlString.flatMap { string in
+                    URL(string: string)
+                }
+                
+                AsyncImage(url: url) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                } placeholder: {
+                    Color(.systemGray4)
+                }
+            }
+            .overlay(alignment: .bottomLeading) {
+                Text(park.name)
+                    .font(.title)
+                    .bold()
+                    .foregroundStyle(.white)
+                    .padding()
+            }
+            .clipShape(.rect(cornerRadius: 16, style: .continuous))
+            .padding(.horizontal)
+    }
 }
