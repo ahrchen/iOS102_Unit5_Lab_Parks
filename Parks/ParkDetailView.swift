884efc6f39e967ad36e277cf02274595cb3fe15a
Raymond Chen <ahrchen@gmail.com>
2024-07-29 19:53:15 -0700

    Add a map to the detail view

--- Parks/ParkDetailView.swift
+++ Parks/ParkDetailView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ParkDetailView: View {
     
@@ -45,7 +46,20 @@ struct ParkDetailView: View {
             }
             .scrollTargetBehavior(.viewAligned)
             .scrollIndicators(.hidden)
+            
+            if let latitude = Double(park.latitude), let longitude = Double(park.longitude) {
+                let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                Map(initialPosition: .region(.init(center: coordinate, latitudinalMeters: 1_000_000, longitudinalMeters: 1_000_000))) {
+                    Marker(park.name, coordinate: coordinate)
+                        .tint(.purple)
+                }
+                .aspectRatio(1, contentMode: .fill)
+                .clipShape(.rect(cornerRadius: 12, style: .continuous))
+                .padding()
+                
+            }
         }
+        
     }
 }
 
